9a6e337a8c1aa2936452805d426fb10ae5633baa
ztian <zhu.weitian@foxmail.com>
2017-10-25 00:07:25 +0800

    nav-simple.html js css

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -2,14 +2,7 @@
 * @Author: ztian
 * @Date:   2017-10-24 11:18:24
 * @Last Modified by:   ztian
-* @Last Modified time: 2017-10-24 22:46:17
+* @Last Modified time: 2017-10-24 23:49:35
 */
 'use strict'
-console.log('hello index');
-require('./index.css');
-var _mm=require('util/mm.js');
-var html = '<div>{{info}}</div>';
-var data={
-    info:'info'
-};
-console.log(_mm.renderHtml(html,data));
+require('page/common/nav-simple/index.js');
