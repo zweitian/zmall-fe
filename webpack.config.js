ba9e47658d1cac73f946f568080afe3f46ee4d4f
ztian <zhu.weitian@foxmail.com>
2017-10-24 16:28:52 +0800

    webpack-dev-server init

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,16 @@
 * @Author: ztian
 * @Date:   2017-10-24 11:19:12
 * @Last Modified by:   ztian
-* @Last Modified time: 2017-10-24 16:03:40
+* @Last Modified time: 2017-10-24 16:23:59
 */
 var webpack                = require('webpack');
-var ExtractTextPlugin      = require("extract-text-webpack-plugin");
-var HtmlWebpackPlugin      = require('html-webpack-plugin');
+ var ExtractTextPlugin      = require("extract-text-webpack-plugin");
+ var HtmlWebpackPlugin      = require('html-webpack-plugin');
+ //获取webpack-dev-server环境变量
+ var WEBPACK_ENV            = process.env.WEBPACK_ENV || 'dev';
+ console.log(WEBPACK_ENV);
  //返回HtmlWebpackPlugin所需html配置
-function getHtmlConfig(name){
+ function getHtmlConfig(name){
     return {
             template    : './src/view/'+ name +'.html',//文件模版地址
             filename    : 'view/'+ name +'.html',//输出文件地址
@@ -16,31 +19,40 @@ function getHtmlConfig(name){
             hash        : true,
             chunks      : ['common',name]//引入common模块js和name模块的js
     }
-}
-var config = {
-    //js入口文件，使用webpack自带js-loader，使用对象数组输入多个需要被转义的js文件
-    entry: {
+ }
+ //webpack config
+ var config = {
+     entry: {
         'common':['./src/page/common/index.js'],
         'index':['./src/page/index/index.js'],
         'login':['./src/page/login/login.js']
-    },
-    //目标文件
-    output: {
-        path: './dist',
-        filename: 'js/[name].js'
-    },
-    //外部依赖
+     },
+     output: {
+         path: './dist',
+         publicPath : '/dist',
+         filename: 'js/[name].js'
+     },
+     //jquery全局变量设置
      externals: {
-        //可以模块化的形式引入全局的jquery
-        'jquery': 'window.jQuery'
+        jquery: 'window.jQuery'
     },
     module: {
         loaders: [
             //读取css的loader
-            { test: /\.css$/, loader: ExtractTextPlugin.extract("style-loader","css-loader") },//从右左执行，先执行css-loader再执行style-loader
+            { test: /\.css$/, loader: ExtractTextPlugin.extract("style-loader","css-loader") },
             //图片、字体文件loader
             { test: /\.(gif|png|jpg|woff|svg|eot|ttf)\??.*$/, loader: 'url-loader?limit=100&name=resource/[name].[ext]' },
         ]
+    },
+    //配置路径别名
+    resolve: {
+        alias : {
+            node_modules    : __dirname + '/node_modules',
+            util            : __dirname + '/src/util',
+            page            : __dirname + '/src/page',
+            service         : __dirname + '/src/service',
+            image           : __dirname + '/src/image',
+        }
     },
     plugins:[
         //公共模块提取插件 所有文件中都被引用到的模块与common模块会被打包成base.js的公共js
@@ -48,10 +60,16 @@ var config = {
             name : 'common',
             filename : 'js/base.js'
         }),
+        //css单独打包插件
         new ExtractTextPlugin("css/[name].css"),
         //js文件与js文件引用的css文件打包进模版html中
         new HtmlWebpackPlugin(getHtmlConfig('index')),
-        new HtmlWebpackPlugin(getHtmlConfig('login'))
+        new HtmlWebpackPlugin(getHtmlConfig('login')),
     ]
-};
-module.exports = config;
+ };
+ //开发环境为dev webpack-dev-server打包入common模块中
+ if('dev' === WEBPACK_ENV){
+    config.entry.common.push('webpack-dev-server/client?http://localhost:8088/');
+ }
+ 
+ module.exports = config;
