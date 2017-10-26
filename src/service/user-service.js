516fc1035977ac72b33fae7c6800989668a3c3f9
ztian <zhu.weitian@foxmail.com>
2017-10-26 13:59:48 +0800

    nav.js user-service cart-service

---
+++ src/service/user-service.js
@@ -0,0 +1,28 @@
+/*
+* @Author: ztian
+* @Date:   2017-10-26 11:46:34
+* @Last Modified by:   ztian
+* @Last Modified time: 2017-10-26 13:36:50
+*/
+'use strict';
+var _mm = require('util/mm.js');
+var _user = {
+     checkLogin : function(resolve,reject){
+        _mm.request({
+            url     : _mm.getServerUrl('/user/get_user_info.do'),
+            method  : 'POST',
+            success : resolve,
+            error   : reject
+        });
+    },
+    //用户登出
+    logout : function(resolve,reject){
+        _mm.request({
+            url     : _mm.getServerUrl('/user/logout.do'),
+            method  : 'POST',
+            success : resolve,
+            error   : reject
+        });
+    }
+};
+modules.exporst = _user;
