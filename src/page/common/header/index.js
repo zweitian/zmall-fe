8912ac5728b542c6fab18acb421cf3db2ea0f504
ztian <zhu.weitian@foxmail.com>
2017-10-26 15:21:17 +0800

    head.html head.css

---
+++ src/page/common/header/index.js
@@ -0,0 +1,21 @@
+/*
+* @Author: ztian
+* @Date:   2017-10-26 14:33:15
+* @Last Modified by:   ztian
+* @Last Modified time: 2017-10-26 14:36:47
+*/
+'use strict'
+require('./index.css');
+//引用通用工具类
+var _mm     = require('util/mm.js');
+//通用页面头部
+var header = {
+    init : function(){
+        this.bindEvent();
+    },
+    //绑定事件
+    bindEvent : function(){
+
+    }
+};
+header.init();
--- src/page/index/index.js
+++ src/page/index/index.js
@@ -2,7 +2,7 @@
 * @Author: ztian
 * @Date:   2017-10-24 11:18:24
 * @Last Modified by:   ztian
-* @Last Modified time: 2017-10-26 14:10:26
+* @Last Modified time: 2017-10-26 14:37:33
 */
 'use strict'
-require('page/common/nav/index.js');
+require('page/common/header/index.js');
