6f9f27560e4b6d95fb76f04bd67763a0dbdf1ea9
ztian <zhu.weitian@foxmail.com>
2017-10-29 15:37:39 +0800

    banner.string banner.js banner.css

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -2,14 +2,32 @@
 * @Author: ztian
 * @Date:   2017-10-24 11:18:24
 * @Last Modified by:   ztian
-* @Last Modified time: 2017-10-26 21:07:44
+* @Last Modified time: 2017-10-29 13:52:28
 */
 'use strict'
+require('./index.css');
 //引入头部导航条(登录 注册)
 require('page/common/nav/index.js');
 //引入头部搜索条(input框,搜索按钮)
 require('page/common/header/index.js');
-//引入带侧边导航条的内容面板
-var navSide=require('page/common/nav-side/index.js');
-//设置order-list为选中状态
-navSide.init({name:'pass-update'});
+//引入banner轮播图
+require('util/slider/index.js');
+//工具类引入
+var _mm             =require('util/mm.js');
+//轮播图模版引入
+var templateBanner  =require('./banner.string');
+$(function(){
+    //渲染轮播图
+    var bannerHtml  =_mm.renderHtml(templateBanner);
+    $('.banner-con').html(bannerHtml);
+    var $slider     = $('.banner').unslider({
+        dots:true
+    });
+    //前一张,后一张按钮事件绑定
+    $('.banner-con .banner-arrow').click(function(){
+        //根据是否有prev判断是上一张还是下一张
+        var forward = $(this).hasClass('prev') ? 'prev':'next';
+        //绑定unslider的上一张,下一张的方法
+        $slider.data('unslider')[forward]();
+    });
+});
---
+++ src/util/slider/index.js
@@ -0,0 +1,9 @@
+/*
+* @Author: ztian
+* @Date:   2017-10-29 10:46:54
+* @Last Modified by:   ztian
+* @Last Modified time: 2017-10-29 10:49:05
+*/
+'use strict'
+require('./index.css');
+require('./unslider.js');
